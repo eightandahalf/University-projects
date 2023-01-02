b3c4f2b7740509cd67e769a82d82054fb15b59d7
Hleb Arbuzau <103840647+eightandahalf@users.noreply.github.com>
2023-01-02 16:23:22 +0300

    Delete University-projects-main directory

--- University-projects-main/trposu_4sem_4work/File.cpp
+++
@@ -1,8 +0,0 @@
-#include "File.h"
-
-File::File(int file_size, std::string file_name, std::string file_type)
-{
-	FI_file_size = file_size;
-	FI_file_name = file_name;
-	FI_file_type = file_type;
-}
--- University-projects-main/trposu_4sem_4work/Random.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RANDOM_H
-#define _RANDOM_H
-
-#include <random>
-
-std::random_device dev; // Will be used to obtain a seed for the random number engine
-std::mt19937 rng(dev()); // Standard mersenne_twister_engine seeded with rd()
-
-#endif // !_RANDOM_H
