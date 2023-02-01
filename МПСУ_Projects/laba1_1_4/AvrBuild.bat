c149d8a056c6ea454c4d10942c53b81d3efbcd28
Hleb Arbuzau <103840647+eightandahalf@users.noreply.github.com>
2023-02-01 17:17:42 +0300

    Add files via upload

---
+++ МПСУ_Projects/laba1_1_4/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"D:\Programs\Creativity\AVRStudio4\AvrAssembler2\avrasm2.exe" -S "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\labels.tmp" -fI -W+ie -C V2E -o "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\laba1_1_4.hex" -d "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\laba1_1_4.obj" -e "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\laba1_1_4.eep" -m "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\laba1_1_4.map" "E:\BSUIR\Some subjects\МПСУ\МПСУ_Projects\laba1_1_4\laba1_1_4.asm"
